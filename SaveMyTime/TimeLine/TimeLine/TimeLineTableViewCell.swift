149c16616cca1e43a6fd49971bf55cc096c2c47d
Fred Zhang <fredzqm@gmail.com>
2018-05-10 01:15:25 -0400

    Add TimeLineView storyboard

---
+++ SaveMyTime/TimeLine/TimeLine/TimeLineTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  TimeLineTableViewCell.swift
+//  SaveMyTime
+//
+//  Created by Fred Zhang on 5/10/18.
+//  Copyright © 2018 Yuan zhou. All rights reserved.
+//
+
+import UIKit
+
+class TimeLineTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var logoImageView: UIImageView!
+    
+    @IBOutlet weak var CategoryNameLabel: UILabel!
+    @IBOutlet weak var TimeSpanLabel: UILabel!
+    @IBOutlet weak var numActionLabel: UILabel!
+    @IBOutlet weak var timeLengthLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
